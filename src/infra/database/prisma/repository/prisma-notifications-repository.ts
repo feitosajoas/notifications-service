d2631aad1411d4ec3813dcbdd021b73e022b9e2c
Joás Feitosa <joasfeitosadelimasilva@gmail.com>
2023-01-13 19:25:53 -0300

    Implementação do conceito de mappers

--- src/infra/database/prisma/repository/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repository/prisma-notifications-repository.ts
@@ -1,6 +1,7 @@
 import { Notification } from '@application/entities/notification';
 import { NotificationsRepository } from '@application/repository/notification-repository';
 import { Injectable } from '@nestjs/common/decorators';
+import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper';
 import { PrismaService } from '../prisma.service';
 
 @Injectable()
@@ -8,14 +9,10 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
 
   async create(notification: Notification): Promise<void> {
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
     await this.prismaService.notification.create({
-      data: {
-        id: notification.id,
-        category: notification.category,
-        content: notification.content.value,
-        recipientId: notification.recipientId,
-        createdAt: notification.createdAt,
-      },
+      data: raw,
     });
   }
 }
